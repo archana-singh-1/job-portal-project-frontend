fabefb3c793e6af56f87e499a8a3fcf0e64c531a
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-10-24 15:49:24 +0530

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Signup from "./components/Signup.jsx"
-import './App.css'
-import Navbar from './components/Navbar.jsx'
-import LandingPage from './components/LandingPage.jsx'
-import Footer from './Footer.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from "./components/Signup.jsx"; 
+import Navbar from './components/Navbar.jsx';
+import LandingPage from './components/LandingPage.jsx';
+import Footer from './Footer.jsx';
 
 function App() {
-
   return (
-    // <div className="bg-blue-500 text-white p-4">
-    //   Hello, Tailwind CSS!
-    // </div>
     <>
-       <Navbar/>
-       <LandingPage/>
-       <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Footer />
     </>
-   
-  )
+  );
 }
 
 export default App;
